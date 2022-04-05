f4fc0e7a24c1f5ca64f00c1dfc0407760d65e937
jdeepee <joshuadparkin@gmail.com>
2022-04-05 13:01:10 +0100

    add basic ConstructorIcon

--- ConstructorIcon.svelte
+++ ConstructorIcon.svelte
@@ -1,10 +1,18 @@
 <svelte:options tag={null}/>
 
 <script lang="ts">
+    //Function injected by ad4m-executor that can be called to publish expression data
+    export let commitExpression;
+
+    let expData = {
+        name: ""
+    };
 </script>
 
 <div>
-    <p>Your UI to create the expression here</p>
+    <p>Create an expression</p>
+    <textarea bind:value={expData.name} />
+    <button on:click={()=>commitExpression(JSON.stringify(expData))}>Commit</button>
 </div>
 
 
